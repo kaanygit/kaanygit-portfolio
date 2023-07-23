4442ba71c22158d996c1bdf5062ca6432bd7afcc
kaanygit <yasinkaan345@hotmail.com>
2023-07-23 04:39:17 +0300

    updated about.ts files

--- src/redux/darkmode/darkmode.reducer.ts
+++ src/redux/darkmode/darkmode.reducer.ts
@@ -11,7 +11,7 @@ type DarkModeAction = {
   };
 
 const initialState={
-    darkmode:false
+    darkmode:true
 }
 
 
--- src/routes/about/about.component.tsx
+++ src/routes/about/about.component.tsx
@@ -3,13 +3,15 @@ import {Outlet} from 'react-router';
 const About:FC=()=>{
     return(
         <>
-            <div className='about-page justify-center text-center items-center p-16 h-screen mx-0 my-auto transition-all duration-500 ease dark:bg-dark-color-1 dark:text-text-color-4 dark:shadow-deneme'>
-                <h1 className='about-heading font-bold text-5xl mb-3'>I'm Yasin Kaan Yigit</h1>
-                <hr className='mb-5'/>
-                <p className='mb-5 text-xl leading-6'>As a mathematics student at Ege University, I have always had a passion for problem-solving and critical thinking. Recently, I have become interested in the world of software development and have been working hard to develop my skills in this field.</p>
-                <p className='mb-5 text-xl leading-6'>I have experience and knowledge in various programming languages, including Python, C# and JavaScript, and I am engaged in both front-end and back-end development.</p>
-                <p className='mb-5 text-xl leading-6'>I enjoy tackling complex problems and finding creative solutions, and I believe my background in mathematics has given me a strong foundation for logical thinking and analytical reasoning</p>
-                <p className='mb-5 text-xl leading-6'>I am excited to continue exploring the world of software development and am always looking for new opportunities to grow and develop my skills.</p>
+            <div className='about-page justify-center text-center items-center p-16 h-screen  transition-all duration-500 ease dark:bg-dark-color-1 dark:text-text-color-4 dark:shadow-deneme'>
+                <div className='w-full h-full'>
+                    <h1 className='about-heading font-bold text-5xl mb-3'>I'm Yasin Kaan Yigit</h1>
+                    <hr className='mb-5'/>
+                    <p className='mb-5 text-xl leading-6'>As a mathematics student at Ege University, I have always had a passion for problem-solving and critical thinking. Recently, I have become interested in the world of software development and have been working hard to develop my skills in this field.</p>
+                    <p className='mb-5 text-xl leading-6'>I have experience and knowledge in various programming languages, including Python, C# and JavaScript, and I am engaged in both front-end and back-end development.</p>
+                    <p className='mb-5 text-xl leading-6'>I enjoy tackling complex problems and finding creative solutions, and I believe my background in mathematics has given me a strong foundation for logical thinking and analytical reasoning</p>
+                    <p className='mb-5 text-xl leading-6'>I am excited to continue exploring the world of software development and am always looking for new opportunities to grow and develop my skills.</p>
+                </div>
                 <Outlet/>
             </div>
         </>
