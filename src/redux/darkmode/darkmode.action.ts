4a3552c1618cdc5df37266e23d2a6ab7c6753315
kaanygit <yasinkaan345@hotmail.com>
2023-09-25 20:40:25 +0300

    template files and integrated on next.js 13

--- src/redux/darkmode/darkmode.action.ts
+++
@@ -1,8 +0,0 @@
-import { DARK_MODE_TYPES, DarkModeAction } from "./darkmode.types";
-
-
-
-export const setDarkMode=(darkmode:boolean):DarkModeAction=>({
-    type:DARK_MODE_TYPES.SET_DARK_MODE,
-    payload:darkmode,
-})
--- src/redux/projects/projetcs.action.ts
+++
@@ -1,9 +0,0 @@
-import { PROJECTS_TYPES, ProjectsData, SetProductsAction } from "./projects.types";
-
-
-
-
-export const setProducts=(products:ProjectsData[]):SetProductsAction=>({
-    type:PROJECTS_TYPES.SET_PROJECTS,
-    payload:products
-})
